e5e7e60bc92c82cba4f1ad2951a23a507fc4a4d9
April Wright <wright.aprilm@gmail.com>
2013-10-02 11:42:17 -0500

    added script for analyzing stripped files

---
+++ h_test_no_para.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import dendropy
+from dendropy.utility.fileutils import find_files
+import sys
+
+d = dendropy.DataSet()
+o_file = sys.argv[1]
+flist = find_files(top=o_file, filename_filter="*")
+trees = dendropy.TreeList()
+for tree_file in flist:
+    trees.read_from_path(tree_file,'nexus')
+d.attach_taxon_set()
+d.read_from_path(tree_file, "nexus")	
+print(d.taxon_sets[0].description(2))    
+
+#H1 = helotrophidae & paraplea in in-group
+h1 = 'Helotrophi', 'Enithares', 'Nerthra sp', 'Ochterus m', 'Ilyocoris', 'Aphelochei','Laccotreph', 'Diplonychu'
+
+#H2: helo in outgroup, but paraplea in in-group
+h2 = 'Enithares', 'Nerthra sp', 'Ochterus m', 'Ilyocoris', 'Aphelochei','Laccotreph', 'Diplonychu'
+
+
+split1 = trees.frequency_of_split(labels=h1)
+print('Frequency of split %s: %s' % (h1, split1))
+
+split2 = trees.frequency_of_split(labels=h2)
+print('Frequency of split %s: %s' % (h2, split2))
