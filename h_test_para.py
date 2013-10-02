44fdc68a1ee1d149f6136ee79f4999f74f9b713a
April Wright <wright.aprilm@gmail.com>
2013-10-02 11:41:27 -0500

    added file for analyzing splits post-estimation

--- h_test_para.py
+++ h_test_para.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env python
 
 import dendropy
-from dendropy import fileutils
+from dendropy.utility.fileutils import find_files
+import sys
 
+d = dendropy.DataSet()
+o_file = sys.argv[1]
 flist = find_files(top=o_file, filename_filter="*")
-for f in f_list:
-	trees.read_from_path(f, 'nexus')
+trees = dendropy.TreeList()
+for tree_file in flist:
+    trees.read_from_path(tree_file,'nexus')
+d.attach_taxon_set()
+d.read_from_path(tree_file, "nexus")	
+print(d.taxon_sets[0].description(2))    
 
 #H1 = helotrophidae & paraplea in in-group
-h1 = ['Nerthra_sp____','Ochterus_marginatus___','Diplonychus_rusticus___','Laccotrephes_robustus___','Paraplea_sp____','Helotrophidae_sp____','Aphelocheirus_ellipsoideus___','Ilyocoris_cimicoides___','Enithares_tibialis___']
+h1 = 'Paraplea s','Helotrophi', 'Enithares', 'Nerthra sp', 'Ochterus m', 'Ilyocoris', 'Aphelochei','Laccotreph', 'Diplonychu'
+
 #H2: helo in outgroup, but paraplea in in-group
-h2 = ['Nerthra_sp____','Ochterus_marginatus___','Diplonychus_rusticus___','Laccotrephes_robustus___','Paraplea_sp____','Aphelocheirus_ellipsoideus___','Ilyocoris_cimicoides___','Enithares_tibialis___']
+h2 = 'Paraplea s','Enithares', 'Nerthra sp', 'Ochterus m', 'Ilyocoris', 'Aphelochei','Laccotreph', 'Diplonychu'
 #Neither in in-group
-h2 = ['Nerthra_sp____','Ochterus_marginatus___','Diplonychus_rusticus___','Laccotrephes_robustus___','Aphelocheirus_ellipsoideus___','Ilyocoris_cimicoides___','Enithares_tibialis___']
+h3 = 'Enithares', 'Nerthra sp', 'Ochterus m', 'Ilyocoris', 'Aphelochei','Laccotreph', 'Diplonychu'
+
+
+
+
+
+split1 = trees.frequency_of_split(labels=h1)
+print('Frequency of split %s: %s' % (h1, split1))
+
+split2 = trees.frequency_of_split(labels=h2)
+print('Frequency of split %s: %s' % (h2, split2))
+
+split3 = trees.frequency_of_split(labels=h3)
+print('Frequency of split %s: %s' % (h3, split3))
